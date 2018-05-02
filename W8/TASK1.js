ec89059d27ab0319d1e106354328b9e319c01473
NRyu <vistlip7@gmail.com>
2018-05-02 13:01:12 +0900

    TASK1

--- W8/TASK1.js
+++ W8/TASK1.js
@@ -32,9 +32,9 @@ function main()
     ];
 
     var scalars = [
-        0,   // S0
-        128, // S1
-        255  // S2
+        0.1,   // S0
+        0.2, // S1
+        0.8  // S2
     ];
 
     // Create color map
@@ -82,9 +82,9 @@ function main()
     for ( var i = 0; i < nfaces; i++ )
     {
         var id = faces[i];
-        var S0 = scalars[ id[0] ];
-        var S1 = scalars[ id[1] ];
-        var S2 = scalars[ id[2] ];
+        var S0 = Math.round((scalars[ id[0] ]-0.1)*(10/7)*255);
+        var S1 = Math.round((scalars[ id[1] ]-0.1)*(10/7)*255);
+        var S2 = Math.round((scalars[ id[2] ]-0.1)*(10/7)*255);
         var C0 = new THREE.Color().setHex( cmap[ S0 ][1] );
         var C1 = new THREE.Color().setHex( cmap[ S1 ][1] );
         var C2 = new THREE.Color().setHex( cmap[ S2 ][1] );
